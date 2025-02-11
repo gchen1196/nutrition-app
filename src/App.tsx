17738aba551e2ce8e81af3379fc8202f5fad6533
George Chen <gchen@zoiclabs.com>
2025-02-11 14:28:33 -0800

    set up shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
     <>
       <h1 className="text-xl font-bold underline">Hello world!</h1>
+      <Button variant='outline'>Click me</Button>
     </>
   );
 }
